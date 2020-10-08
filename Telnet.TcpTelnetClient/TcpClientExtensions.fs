673caaec6afead7d34ae0faa7a2aa13152f431f5
Krissy Miller <kmiller@ctdi.com>
2020-10-08 15:33:58 -0400

    Updated the tcpclient receive extensions to instead of depend on a timeout, just get all the data available. RFC854 seems to suggest that all data will be queued and sent together so this should not cause any problems

--- Telnet.TcpTelnetClient/TcpClientExtensions.fs
+++ Telnet.TcpTelnetClient/TcpClientExtensions.fs
@@ -4,6 +4,18 @@ module KD.Telnet.TcpTelnetClient.TcpClientExtensions
 open System
 
 type System.Net.Sockets.TcpClient with
+
+//There does not seem to be any sort of indication to say all the data has been transmitted to, no special characters, 
+//no special message, not even a length field. The assumption is that all data comes back at once so this.Available has ALL the data.
+//This is suggested in RFC854:
+//...the following default conditions pertain to the transmission of data over the TELNET connection:
+//   1)  Insofar as the availability of local buffer space permits,
+//   data should be accumulated in the host where it is generated
+//   until a complete line of data is ready for transmission, or
+//   until some locally-defined explicit signal to transmit occurs.
+//   This signal could be generated either by a process or by a
+//   human user.
+
     member this.GetEchoAsync(lengthExpected, timeout) = 
         let startTime = DateTime.Now
         let stream = this.GetStream()
@@ -12,7 +24,8 @@ type System.Net.Sockets.TcpClient with
                 let dataLength = Array.length data
 
                 if dataLength = lengthExpected || (DateTime.Now - startTime) >= timeout then
-                    return data
+                    raise (OperationCanceledException())
+                    return Unchecked.defaultof<_>
                 elif dataLength > lengthExpected then
                     return failwith "datalength > lengthExpected"
                 else
@@ -23,9 +36,7 @@ type System.Net.Sockets.TcpClient with
                     | n ->
                         let buffer = Array.zeroCreate (Math.Min(lengthExpected - dataLength, n))
                         let! count = stream.AsyncRead(buffer)
-                        return!
-                            Array.concat [ data ; buffer |> Array.truncate count ]
-                            |> ReceiveDataAsync
+                        return Array.concat [ data ; buffer |> Array.truncate count ]
                 }
 
         ReceiveDataAsync Array.empty
@@ -36,7 +47,8 @@ type System.Net.Sockets.TcpClient with
 
         let rec ReceiveDataAsync data = async {
                 if (DateTime.Now - startTime) >= timeout then
-                    return data
+                    raise (OperationCanceledException())
+                    return Unchecked.defaultof<_>
                 else
                     match this.Available with
                     | 0 ->
@@ -45,9 +57,7 @@ type System.Net.Sockets.TcpClient with
                     | n ->
                         let buffer = Array.zeroCreate n
                         let! count = stream.AsyncRead(buffer)
-                        return!
-                            Array.concat [ data ; buffer |> Array.truncate count ]
-                            |> ReceiveDataAsync
+                        return Array.concat [ data ; buffer |> Array.truncate count ]
                 }
 
         ReceiveDataAsync Array.empty
